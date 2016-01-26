e61d6158320c28f8493342d8bda23ff1fe2ed5c1
Ian Raphael <ianrapha@gmail.com>
2016-01-25 23:53:44 -0200

    Fibonacci closure exercise.

---
+++ functions/fibonacciclosure.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+    "fmt"
+)
+
+// fibonacci is a function that returns a function that returns an int.
+func fibonacci() func() int {
+    first, second := 0, 1
+
+    return func() (sum int) {
+        sum = first
+        first, second = second, first + second
+
+        return
+    }
+}
+
+func main() {
+    f := fibonacci()
+    for i := 0; i < 10; i++ {
+        fmt.Println(f())
+    }
+}
